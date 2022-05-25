8be84deadd67def74d873e323870f59ea6c22898
Picachune <101989919+Picachune@users.noreply.github.com>
2022-05-26 00:18:35 +0600

    inheritance

--- br5.py
+++ br5.py
@@ -1,1 +1,24 @@
-# checking
+class person:
+    def __init__(self,name,id_number):
+        self.name = name
+        self.id_number = id_number
+
+    def details(self):
+        print('my name is ',self.name)
+        print('my id number is - ',self.id_namber)
+
+class employe(person):
+    def __init__(self,name,id_number,selary,post):
+        self.selary = selary
+        self.post = post
+        #invoking the __init__ of the parent class
+        person.__init__(self,name,id_number)
+
+    def details(self):
+        print('my name is ', self.name)
+        print('my id number is- ', self.id_number)
+        print('my post in ', self.post)
+        print('my selary is ', self.selary)
+
+guru = employe('snake',31,50000,'banglore')
+guru.details()
